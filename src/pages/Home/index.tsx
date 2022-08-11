fc164b4436fecdeecdccac3c061a15611a5b82da
Wellton Quirino <welltonq@outlook.com>
2022-08-11 11:23:31 -0300

    Projeto pomodoro concluído

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,8 @@
 import { HandPalm, Play } from "phosphor-react";
 
-import { differenceInSeconds } from "date-fns";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as zod from "zod";
-import { createContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { NewCycleForm } from "./components/NewCycleForm";
 import { Countdown } from "./components/Countdown";
 
@@ -13,26 +12,7 @@ import {
   ButtonStop
 } from "./styles";
 import { FormProvider, useForm } from "react-hook-form";
-
-
-interface Cycle {
-  id: string;
-  task: string;
-  minutesAmount: number;
-  startDate: Date;
-  interruptedDate?: Date;
-  finishedDate?: Date;
-}
-
-interface CyclesContextType {
-  activeCycle: Cycle | undefined;
-  activeCycleId: string | null;
-  amountSecondsPassed: number;
-  markCurrentCycleAsFinished: () => void;
-  setSecondsPassed: (seconds: number) => void;
-}
-
-export const CyclesContext = createContext({} as CyclesContextType)
+import { CyclesContext } from "../../contexts/CyclesContext";
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
@@ -45,11 +25,7 @@ const newCycleFormValidationSchema = zod.object({
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
-  const [cycles, setCycles] = useState<Cycle[]>([])
-  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
-
-  const activeCycle = cycles.find(cycle => cycle.id === activeCycleId)
+  const { activeCycle, createNewCycle, interruptCurrentCycle } = useContext(CyclesContext)
 
   const newCycleForm = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
@@ -61,79 +37,26 @@ export function Home() {
 
   const { handleSubmit, watch, reset } = newCycleForm
 
-  function setSecondsPassed(seconds: number) {
-    setAmountSecondsPassed(seconds)
-  }  
-
-  function markCurrentCycleAsFinished() {
-    setCycles(state =>
-      state.map(cycle => {
-        if(cycle.id === activeCycleId) {
-          return { ...cycle, finishedDate: new Date() }
-        } else {
-          return cycle
-        }
-      })
-    )
-  }
-  
-  function handleSubmitNewCycle(data: NewCycleFormData) {
-    const id = String(new Date().getTime())
-
-    const newCycle: Cycle = {
-      id,
-      task: data.task,
-      minutesAmount: data.minutesAmount,      
-      startDate: new Date()
-    }
-    
-    setCycles(state => [...state, newCycle])
-    setActiveCycleId(id)
-    setAmountSecondsPassed(0)
-
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    createNewCycle(data)
     reset()
   }
-
-  function handleInterruptCycle() {
-    setCycles(state => state.map(cycle => {
-        if(cycle.id === activeCycleId) {
-          return { ...cycle, interruptedDate: new Date() }
-        } else {
-          return cycle
-        }
-      })
-    )
-
-    setActiveCycleId(null)    
-  }
-
-  
   
   const task = watch('task');
   const isSubmitDisabled = !task;
 
   return (
     <HomeContainer>
-      <form onSubmit={handleSubmit(handleSubmitNewCycle)}>
-
-        <CyclesContext.Provider 
-          value={{
-            activeCycle, 
-            activeCycleId, 
-            markCurrentCycleAsFinished, 
-            amountSecondsPassed, 
-            setSecondsPassed
-          }}>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
           <FormProvider {...newCycleForm}>
             <NewCycleForm />
           </FormProvider>
           <Countdown />
-        </CyclesContext.Provider>
         
         
 
         {activeCycle ? (
-          <ButtonStop onClick={handleInterruptCycle} type="button">
+          <ButtonStop onClick={interruptCurrentCycle} type="button">
             <HandPalm size={24} />
               Interromper
           </ButtonStop>
